ea55486aa4cee91dc4a583abd14e98f45d9bc885
yassineelhoubi <elhoubiyassine@gmail.com>
2022-01-07 21:17:26 +0100

    remove event listener

--- src/assets/js/style.js
+++ src/assets/js/style.js
@@ -9,6 +9,3 @@ btn.addEventListener("click", () => {
     changeIcon = !changeIcon
     changeIcon ? btn.innerHTML = `<i class="far fa-window-close"></i>` : btn.innerHTML = `<i class="fas fa-bars"></i>`
 })
-document.querySelector(".dashContainer").addEventListener("click", () => {
-    console.log("sqdqsd")
-})
