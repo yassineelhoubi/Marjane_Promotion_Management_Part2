0ad74e6313ace9dd7b9f16f22823bfe3496764a3
yassineelhoubi <elhoubiyassine@gmail.com>
2022-01-09 18:02:09 +0100

    make body hidden before log

--- src/app/services/admin/isAdmin.js
+++ src/app/services/admin/isAdmin.js
@@ -1,15 +1,18 @@
 
-function isAdmin(){
-    obj ={
-        token : localStorage.getItem('token'),
-        role : "ADMIN"
+function isAdmin() {
+    obj = {
+        token: localStorage.getItem('token'),
+        role: "ADMIN"
     }
-    axios.post('http://localhost:3000/api/admin/checkAuth',obj)
-    .then((res)=>{
-        if (res.data.isLogged == false ) {
-            document.location.href = './login.html'
-          }
-    })
-    
+    axios.post('http://localhost:3000/api/admin/checkAuth', obj)
+        .then((res) => {
+            if (res.data.isLogged == false) {
+                document.location.href = './login.html'
+            }
+            else {
+                document.querySelector('body').classList.remove('hidden')
+            }
+        })
+
 }
 isAdmin();
